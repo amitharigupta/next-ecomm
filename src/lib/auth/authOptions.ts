3f9cebcd46db5802cb04f3bfe48ac40d3bf5da1f
Amit Gupta <amitg@VF096.local>
2024-07-21 17:22:21 +0530

    working on login functionality

--- src/lib/auth/authOptions.ts
+++ src/lib/auth/authOptions.ts
@@ -1,5 +1,16 @@
-export const authOptions = {
-  providers: {
+import GoogleProvider from "next-auth/providers/google";
 
-  }
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET_KEY as string,
+      async profile(profile, token: any) {
+        console.log("profile", profile);
+        console.log("tokens", token);
+
+        return { id: profile.id }
+      }
+    })
+  ]
 }
