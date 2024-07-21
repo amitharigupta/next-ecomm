09235c9d962306280223d8b055eaee6f584d0073
Amit Gupta <amitg@VF096.local>
2024-07-21 14:56:24 +0530

    added shad cn ui library in project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <h1>Ecomm</h1>
+    <>
+      <h1 className="font-semibold text-green-500">Ecomm</h1>
+      <Button variant={"outline"} size={"lg"}>Click Me</Button>
+    </>
   );
 }
