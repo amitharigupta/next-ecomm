5d974d3901c840086bd3f439a6e87ef14db24729
Amit Gupta <amitg@VF096.local>
2024-07-21 10:58:58 +0530

    Warehosuses get all warehouses endpoint

--- src/app/api/warehouses/route.ts
+++ src/app/api/warehouses/route.ts
@@ -19,5 +19,17 @@ export async function POST(request: Request, response: Response) {
   } catch (error) {
     return Response.json({ status: 500, message: "Error while creating warehouses" + error }, { status: 500 })
   }
+}
 
+export async function GET() {
+  try {
+    const allWarehouses = await db.select().from(warehouses);
+    if (allWarehouses.length > 0) {
+      return Response.json({ status: 200, message: "All warehouses fetched successfully", data: allWarehouses }, { status: 200 })
+    } else {
+      return Response.json({ status: 404, message: "No warehouses found" }, { status: 404 })
+    }
+  } catch (error) {
+    return Response.json({ status: 500, message: "Error while fetching warehouses" + error }, { status: 500 })
+  }
 }
